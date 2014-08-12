cef1daa83f234ef2706d4b2d48c6fee66b63fb81
Seth Archambault <seth@doercreator.com>
2014-08-11 22:23:29 -0400

    removed delete

--- app/views/admin/bike_index.blade.php
+++ app/views/admin/bike_index.blade.php
@@ -9,13 +9,9 @@
 				<th>bike_uid</th>
 				<th>description</th>
 				<th>status</th>
-				<th>photo</th>
 				<th>lost_date</th>
-				<th>found_date</th>
-				<th>found_story</th>
 				<th>email</th>
 				<th></th>
-                <th></th>
 			</tr>
 			@foreach ($bikes as $bike)
 			<tr>
@@ -23,13 +19,9 @@
 				<td>{{ $bike->bike_uid }}</td>
 				<td>{{ $bike->description }}</td>
 				<td>{{ $bike->status }}</td>
-				<td>{{ $bike->photo }}</td>
 				<td>{{ $bike->lost_date }}</td>
-				<td>{{ $bike->found_date }}</td>
-				<td>{{ $bike->found_story }}</td>
 				<td>{{ $bike->email }}</td>
 				<td><a href="/admin/bike_edit/{{ $bike->id }}">Edit</a></td>
-                <td><a href="/admin/bike_delete/{{ $bike->id }}">Delete</a></td>
             </tr>
 			@endforeach
 			
